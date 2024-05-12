85051931d6160b40e6cd9cbe771f987ca94cbe2c
Kapil Arora <2754963a@student.gla.ac.uk>
2024-05-12 12:18:19 +0530

    learning the use of embedded javascript aka ejs

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+
+app.set('view engine', 'ejs')
+
 const app = new express()
 
 app.use(express.static('public'))
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
+})
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
 })
